d8bfdce85e9f38826bedc843fa6007ae872e2b3b
Mokka <killmoregirls@gmail.com>
2024-12-22 19:00:29 +0100

    Update application names in tomls. Add solution for 3.1.

--- day-03-01/src/main.rs
+++ day-03-01/src/main.rs
@@ -1,3 +1,28 @@
+mod data;
+
+use data::INPUT as text;
+use regex::Regex;
+
 fn main() {
-    println!("Hello, world!");
+    let expression = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+
+    let result = expression
+        .captures_iter(text)
+        .map(|captures| {
+            (
+                captures.get(1).unwrap().as_str(),
+                captures.get(2).unwrap().as_str(),
+            )
+        })
+        .map(|pair| {
+            (
+                pair.0.parse::<i32>().unwrap_or(0),
+                pair.1.parse::<i32>().unwrap_or(0),
+            )
+        })
+        .map(|pair| pair.0 * pair.1)
+        .reduce(|acc, element| acc + element)
+        .unwrap_or(0);
+
+    println!("{}", result);
 }
